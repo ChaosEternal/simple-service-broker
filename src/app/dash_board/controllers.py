71cc8e171cf27c04e0213dafaf77c8389c59f986
Chaos Eternal <chaos@shlug.org>
2017-11-01 17:44:47 +0800

    workable with out database

---
+++ src/app/dash_board/controllers.py
@@ -0,0 +1,25 @@
+import json, sys
+
+from flask import Blueprint, request, g, abort, jsonify, render_template
+
+from app import db
+
+from app import app
+
+from app.service_broker.models import CldInfo, SrvcInst, SrvcBind
+from app.service_broker.cloud_info import make_cld_info, normalize_uri
+
+from app.exceptions import *
+
+from app.metadata import *
+
+from app.auth import auth
+
+dash_board = Blueprint('dshbrd', __name__, url_prefix="/")
+
+@dash_board.route(app.config["SERVICE_INSTANCE_URI"].format(instance_id='<instance_id>'), methods=['GET'])
+def dashboard(instance_id):
+    cld = CldInfo
+    srvc = SrvcInst
+    binding = SrvcBind
+    return render_template("dashboard.html", cld=cld, srvc= srvc, binding = binding)
